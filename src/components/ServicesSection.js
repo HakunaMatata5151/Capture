7bafc7c76168d4f40a9c230ce845a05e827d7604
HakunaMatata5151 <saadtariq5151@gmail.com>
2021-07-18 07:14:43 +0500

    6. Styling Services Section

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -1,57 +1,86 @@
 import React from "react";
+import styled from "styled-components";
 //Import icons
 import clock from "../img/clock.svg";
 import diaphragm from "../img/diaphragm.svg";
 import money from "../img/money.svg";
 import teamwork from "../img/teamwork.svg";
 import home2 from "../img/home2.png";
+//importing styled components
+import { Hide, About, Description, Image } from "../styles";
 
 function ServicesSection() {
   return (
-    <div className="services">
-      <div className="desription">
+    <Services>
+      <Description>
         <h2>
           High <span>quaity</span>services
         </h2>
-        <div className="cards">
-          <div className="card">
+        <Cards>
+          <Card>
             <div className="icon">
               <img src={clock} alt="A clock" />
               <h3>Efficient</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
+          </Card>
 
-          <div className="card">
+          <Card>
             <div className="icon">
               <img src={teamwork} alt="A teamwork" />
               <h3>TeamWork</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
+          </Card>
 
-          <div className="card">
+          <Card>
             <div className="icon">
               <img src={diaphragm} alt="A diaphragm" />
               <h3>Diaphgram</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
+          </Card>
 
-          <div className="card">
+          <Card>
             <div className="icon">
               <img src={money} alt="A money" />
               <h3>Affordable</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-        </div>
-      </div>
-      <div className="img">
+          </Card>
+        </Cards>
+      </Description>
+      <Image>
         <img src={home2} alt="" />
-      </div>
-    </div>
+      </Image>
+    </Services>
   );
 }
+const Services = styled(About)`
+  h2 {
+    padding-bottom: 5rem;
+  }
+  p {
+    width: 70%;
+    padding: 2rem 0rem 4rem 0rem;
+  }
+`;
+const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+const Card = styled.div`
+  flex-basis: 20rem;
+  .icon {
+    display: flex;
+    align-items: center;
+    h3 {
+      margin-left: 1rem;
+      background: white;
+      color: black;
+      padding: 1rem;
+    }
+  }
+`;
 
 export default ServicesSection;
