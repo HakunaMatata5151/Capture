af376eab67c553e7fd8346f2166e54ea01087736
HakunaMatata5151 <saadtariq5151@gmail.com>
2021-07-19 12:45:20 +0500

    12. Framer Motion

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,28 +1,43 @@
 import React from "react";
 import home1 from "../img/home1.png";
-//import styled
-import styled from "styled-components";
+//importing framer Motion
+import { motion } from "framer-motion";
+
 //import About,description and image and hide from the styles so for the sake of the ease
 
 import { Hide, About, Description, Image } from "../styles";
 
 function AboutSection() {
+  // const titleAnim = {
+  //   hidden: { opacity: 0 },
+  //   show: { opacity: 1, transition: { duration: 2 } },
+  // };
+  // const container = {
+  //   hidden: { x: 100 },
+  //   show: {
+  //     x: 0,
+  //     transition: { duration: 1, staggerChildren: 0.7, when: "afterChildren" },
+  //   }, // can also use ease out which removes the jerk operation
+  // };
   return (
     <About>
       <Description>
-        <div className="title">
+        <motion.div
+          variants={container}
+          initial="hidden"
+          animate="show"
+          className="title"
+        >
+          <motion.h2 /*variants={titleAnim}*/>We work to make</motion.h2>
           <Hide>
-            <h2>we work to made</h2>
-          </Hide>
-          <Hide>
-            <h2>
+            <motion.h2 /*variants={titleAnim}*/>
               Your <span>dreams</span> come
-            </h2>
+            </motion.h2>
           </Hide>
           <Hide>
-            <h2>True.</h2>
+            <motion.h2 /*variants={titleAnim}*/>True.</motion.h2>
           </Hide>
-        </div>
+        </motion.div>
         <p>
           Contact us for any photgraphy videography ideas that u have. We have
           professionals with amazing skills.
